aca3b3ca45190570c81b251ae3d700b4505bbeaf
Joe Davies <joedpwd@hotmail.co.uk>
2020-06-05 14:57:08 +0100

    Creating basic October CMS theme

---
+++ theme/assets/js/main.js
@@ -0,0 +1,22 @@
+Vue.component('post-card', {
+    template: `
+    <div class="column is-half-tablet is-one-quarter-desktop">
+        <div class="content-card card">
+            <slot name="post-image"></slot>
+            <div class="card-content">
+                <h3 class="is-size-3"><slot name="post-title"></slot></h3>
+                <p><slot name="post-summary"></slot></p>
+            </div>
+        </div>
+    </div>
+    `
+});
+
+
+new Vue({
+    el: '#root',
+
+    data: {
+        showNav: false
+    }
+});
