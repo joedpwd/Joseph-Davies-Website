87882ec535968d74b32427685d76250cc5693130
Joe Davies <joedpwd@hotmail.co.uk>
2020-06-09 15:43:39 +0100

    Adding partial/styles for blog post list on the front page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 Vue.component('post-card', {
     template: `
-    <div class="column is-half-tablet is-one-quarter-desktop">
+    <div class="column is-half-tablet is-one-third-desktop is-one-quarter-fullhd">
         <div class="content-card card">
             <slot name="post-image"></slot>
             <div class="card-content">
