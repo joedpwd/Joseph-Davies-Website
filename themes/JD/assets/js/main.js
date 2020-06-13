367149693fe4efe17de774a7979785e3b4eb583c
Joe Davies <joedpwd@hotmail.co.uk>
2020-06-13 16:15:20 +0100

    Further Styling, Hover on cards

--- themes/JD/assets/js/main.js
+++ themes/JD/assets/js/main.js
@@ -1,11 +1,12 @@
 Vue.component('post-card', {
     template: `
     <div class="column is-half-tablet is-one-third-desktop is-one-quarter-fullhd">
-        <div class="content-card card">
+        <div class="card card-container grow">
+            <div
             <slot name="post-image"></slot>
             <div class="card-content">
                 <slot name="post-title"></slot>
-                <p><slot name="post-highlight"></slot></p>
+                <div class="card-excerpt"><slot name="post-highlight"></slot></div>
             </div>
         </div>
     </div>
